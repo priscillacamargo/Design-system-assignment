462ac15836d2c82e5082d39c6eb809d01bbb8cc5
Priscilla Camargo <priscillacamargo@Priscillas-MacBook-Air.local>
2023-11-04 12:25:22 +0100

    Add initial setup for a reactapp with vite, ts, elslint prettier husky and tailwinf

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold text-sky-400 underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
